354d85ee0e28e748a5e5b99dfcb3c51dd1ee7678
cedcoss <harshsharma@cedcoss.com>
2022-03-02 18:40:42 +0530

    template 1 and template 2

---
+++ src/dash.js
@@ -0,0 +1,45 @@
+let data=[
+    {
+        "id":23432,
+        "name":"one",
+        "Categories":"xyz",
+        "item":1
+
+    },
+    {
+        "id":23432,
+        "name":"one",
+        "Categories":"xyz",
+        "item":1
+
+    },
+    {
+        "id":23432,
+        "name":"one",
+        "Categories":"xyz",
+        "item":1
+
+    },
+    {
+        "id":23432,
+        "name":"one",
+        "Categories":"xyz",
+        "item":1
+
+    },
+    {
+        "id":23432,
+        "name":"one",
+        "Categories":"xyz",
+        "item":1
+
+    },
+    {
+        "id":23432,
+        "name":"one",
+        "Categories":"xyz",
+        "item":1
+
+    },
+
+]
